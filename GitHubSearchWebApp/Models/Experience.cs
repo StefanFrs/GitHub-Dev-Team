8202ec2c445ae815aa82c06721f3e00255579905
SofiaHritcu <61913547+SofiaHritcu@users.noreply.github.com>
2021-09-11 19:08:55 +0300

    #GitHubDevTeam - update: added documentation for entities

--- GitHubSearchWebApp/Models/Experience.cs
+++ GitHubSearchWebApp/Models/Experience.cs
@@ -5,14 +5,25 @@ using System.Threading.Tasks;
 
 namespace GitHubSearchWebApp.Models
 {
+    /// <summary>
+    ///   <br />
+    /// </summary>
     public class Experience
     {
+        /// <summary>Gets or sets the identifier.</summary>
+        /// <value>The identifier.</value>
         public int Id { get; set; }
 
+        /// <summary>Gets or sets the description.</summary>
+        /// <value>The description.</value>
         public string Description { get; set; }
 
+        /// <summary>Gets or sets the size of the code.</summary>
+        /// <value>The size of the code.</value>
         public string CodeSize { get; set; }
 
+        /// <summary>Gets or sets the developer identifier.</summary>
+        /// <value>The developer identifier.</value>
         public int DeveloperId { get; set; }
     }
 }
