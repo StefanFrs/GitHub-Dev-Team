8e329e6f1ba4fe2f669ff1cf4edb51e49b2d033a
ionutroth <87963655+ionutroth@users.noreply.github.com>
2021-09-13 03:17:25 +0300

    DONT TOUCH! Fetched languages used by devs
    -structure is a voodooo but it works somehow
    -please do not interfere

--- GitHubSearchWebApp/wwwroot/js/developers.js
+++ GitHubSearchWebApp/wwwroot/js/developers.js
@@ -1,23 +1,13 @@
-﻿var settings = {
+﻿var settingsUsers = {
     "url": "https://localhost:5001/allDevelopers",
     "method": "GET",
     "timeout": 0,
 };
 
-$.ajax(settings).done(function (responses) {
+$.ajax(settingsUsers).done(function (responses) {
     var users = document.getElementsByClassName("users")[0];
     
     responses.forEach((response) => {
-        //const headers = {
-        //        "Authorization":`Token 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`
-        //}
-        //var userLink = "https://api.github.com/users/" + response.gitLogin;
-        //var userResponse = await fetch(userLink, {
-        //    "method": "GET",
-        //    "headers": headers
-        //});
-        //console.log(userResponse)
-        //var userIcon = userResponse["avatar_url"];
 
         var newCard = document.createElement("DIV");
         newCard.classList.add("user-card");
@@ -27,49 +17,91 @@ $.ajax(settings).done(function (responses) {
                                     <div class="user-name">
                                         <h3>${response.fullName}</h3>
                                     </div>
-                                        <div class="git-account-image d-flex">
-                                            <p class="pr-2">${response.gitLogin}</p>
-                                            <img src="images/670020.png">
-                                        </div>
+                                    <div class="git-account-image d-flex">
+                                        <p class="pr-2">${response.gitLogin}</p>
+                                        <img src="images/670020.png">
                                     </div>
-                                    <div class="col-lg-4">
-                                        <div class="card-email">
-                                            <p>${response.email}</p>
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-8 mt-4">
-                                        <div class="skills-list d-flex justify-content-around align-items-center flex-wrap">
-
-                                            <div class="skill-item d-flex align-items-center">
-                                                <div class="dot mr-2">
-                                                    </div>
-                                                    <div class="skill-name ">
-                                                        <p>C#</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-
-                                        </div>
-                                        <div class="col-lg-4 mt-3">
-                                            <button class="btn btn-dark btn-lg button-view"><i class="fas fa-expand-arrows-alt"></i>
-                                                View</button>
-                                        </div>
-                                    </div>
-                                    <hr class="card-hr">
                                 </div>
-                            </div>`;
+                                <div class="col-lg-4">
+                                    <div class="card-email">
+                                        <p>${response.email}</p>
+                                    </div>
+                                </div>
+                                <div class="col-lg-8 mt-4">
+                                    <div class="skills-list d-flex justify-content-around align-items-center flex-wrap">
+                                   
+                                    </div>
+                                <div class="col-lg-4 mt-3">
+                                    <button class="btn btn-dark btn-lg button-view"><i class="fas fa-expand-arrows-alt"></i>
+                                    View</button>
+                                </div>
+                            </div>
+                            <hr class="card-hr">`;
         users.appendChild(newCard);
-        //async function getRepos(user) {
-            
-        //    var repositoriesJson = await repositoriesResponse.json();
-        //    console.log(repositoriesJson);
-            //repositoriesJson.forEach(repo => {
-            //   //var languagesResponse = await fetch(repo["languages_url"]);
-            //   // var languagesJson = await languagesResponse.json();
-            //   // console.log(languagesJson);
-            //})
-        //}
 
-        //getRepos(response.gitLogin);
+        var settingsLanguages = {
+            "url": "https://localhost:5001/api/experiences/programmingLanguages/" + response.gitLogin,
+            "method": "GET",
+            "timeout": 0,
+            "headers": {
+                "Authorization": "Bearer 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"
+            },
+        };
+        $.ajax(settingsLanguages).done(function (response) {
+            var skillsContainer = document.getElementsByClassName("skills-list")[0];
+
+            var skillList = [];
+
+            if (response.length > 5) {
+                skillList = response.slice(0, 4);
+                skillList.push("And" + (response.length - 4) + "other languages")
+            }
+            else {
+                skillList = response.slice(0, 5);
+            }
+
+            skillList.forEach(element => {
+                $.getJSON('js/colours.json', { element: element }).done(function (json) {
+                    var coloursDictionary = [];
+                    for (var language in json) {
+                        if (json.hasOwnProperty(language)) {
+                            var item = json[language];
+                            coloursDictionary.push({
+                                name: language,
+                                colour: item.color
+                            });
+                        }
+                    }
+
+                    var newLanguageColour = "";
+                    var newLanguageObject = [];
+                    if (coloursDictionary.some(e => e.name === element)) {
+                        newLanguageObject = coloursDictionary.filter(obj => {
+                            return obj.name === element;
+                        })
+                        newLanguageColour = newLanguageObject[0].colour;
+                    }
+                    else {
+                        newLanguageColour = "gray";
+                    }
+                    var newLanguage = document.createElement("DIV");
+                    newLanguage.classList.add("skill-item");
+                    newLanguage.classList.add("d-flex");
+                    newLanguage.classList.add("align-items-center");
+                    newLanguage.innerHTML = `<div class="skill-item d-flex align-items-center">
+                                                <div class="dot mr-2" style="background:${newLanguageColour}">
+                                                </div>
+                                                <div class="skill-name ">
+                                                    <p>${element}</p>
+                                                </div>
+                                            </div>`;
+                    skillsContainer.appendChild(newLanguage);
+                });
+            })
+        });
     })
 })
+
+
+
+
