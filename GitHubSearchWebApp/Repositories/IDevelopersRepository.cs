53ca80403afc2076d8c6795c9073431d845585a9
SofiaHritcu <61913547+SofiaHritcu@users.noreply.github.com>
2021-09-14 20:20:32 +0300

    #GitHubDevTeam - add: experience repo interface + db implementation

---
+++ GitHubSearchWebApp/Repositories/IDevelopersRepository.cs
@@ -0,0 +1,13 @@
+﻿using GitHubSearchWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitHubSearchWebApp.Repositories
+{
+    public interface IDevelopersRepository
+    {
+        public Developer GetById(int id);
+    }
+}
