0e5eae52b59489fac7334518d4907bda6b8d6809
ionutroth <87963655+ionutroth@users.noreply.github.com>
2021-09-21 23:50:35 +0300

    Added DaataTools

---
+++ GitHubSearchWebApp/Data/DataTools.cs
@@ -0,0 +1,44 @@
+﻿using GitHubSearchWebApp.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace GitHubSearchWebApp.Data
+{
+    public class DataTools
+    {
+        public static void SeedData(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                var applicationDbContext = serviceScope.ServiceProvider.GetService<ApplicationDbContext>();
+                if (applicationDbContext.Developer.Any())
+                {
+                    Console.WriteLine("The developers are there");
+                }
+                else
+                {
+                    Console.WriteLine("No developers");
+                    var dev1 = new Developer();
+                    var dev2 = new Developer();
+                    dev1.Id = 1;
+                    dev1.FullName = "Ionut";
+                    dev1.GitLogin = "ionutroth";
+                    dev1.Email = "rothionut@mail.com";
+                    dev2.Id = 2;
+                    dev2.FullName = "sofia";
+                    dev2.GitLogin = "SofiaHritcu";
+                    dev2.Email = "sofia@mail.com";
+                    applicationDbContext.Add(dev1);
+                    applicationDbContext.Add(dev2);
+                    applicationDbContext.SaveChanges();
+                }
+            }
+        }
+    }
+}
--- GitHubSearchWebApp/Startup.cs
+++ GitHubSearchWebApp/Startup.cs
@@ -38,7 +38,7 @@ namespace GitHubSearchWebApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                  options.UseNpgsql(GetConnectionString()));
+                  options.UseSqlServer(GetConnectionString()));
             Configuration.GetConnectionString("DefaultConnection");
             services.AddDatabaseDeveloperPageExceptionFilter();
 
@@ -113,6 +113,7 @@ namespace GitHubSearchWebApp
                 endpoints.MapRazorPages();
                 endpoints.MapHub<UpdatesHub>("/updates");
             });
+            DataTools.SeedData(app);
         }
     }
 }
